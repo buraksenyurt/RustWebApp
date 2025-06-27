23f8bc3045763e68c004b79df89dd9ba11084add
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-27 13:47:18 +0300

    Added feature-based library

---
+++ src/planner/dal/src/lib.rs
@@ -0,0 +1,18 @@
+// json_store modülü eğer bu feature ile build edilirse kullanılabilir
+#[cfg(feature = "json-store")]
+pub mod json_store;
+
+// pub fn add(left: u64, right: u64) -> u64 {
+//     left + right
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
