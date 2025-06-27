797782ca067cb476360bd9521402d032f7d58fcc
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-27 12:03:01 +0300

    Integrated clap for base cli ops

--- src/planner/core/src/main.rs
+++ src/planner/core/src/main.rs
@@ -1,5 +1,25 @@
+use clap::Parser;
+
 mod api;
 mod enums;
 mod structs;
 
-fn main() {}
+fn main() {
+    let args = Args::parse();
+    println!("{:?}", args);
+}
+
+/// CLI Tool for planner
+#[derive(Parser, Debug)]
+#[command(version="1.0",about="Planner CLI",long_about = None)]
+struct Args {
+    /// The title of the work item
+    #[arg(short, long)]
+    title: String,
+    /// The size of the work item (1,2,3,5,8,13...)
+    #[arg(short, long)]
+    volume: u8,
+    /// Current status of work item (Ready, InProgress, Completed)
+    #[arg(short, long, default_value = "Ready")]
+    status: String,
+}
