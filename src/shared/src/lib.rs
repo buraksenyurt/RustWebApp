19a1ae1ce8cd88c1b6cf861f7505e5e4a7749de2
buraksenyurt <burakselimsenyurt@gmail.com>
2025-07-03 13:53:27 +0300

    Added unit tests for error type.

--- src/shared/src/lib.rs
+++ src/shared/src/lib.rs
@@ -1,16 +1,75 @@
+use crate::errors::{ServiceError, ServiceErrorStatus};
+
 mod errors;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[allow(dead_code)]
+fn simulate_get_categories(category_id: u32) -> Result<String, ServiceError> {
+    if category_id == 0 {
+        Err(ServiceError::new(
+            ServiceErrorStatus::BadRequest,
+            "categories cannot be 0".to_string(),
+        ))
+    } else if category_id == 404 {
+        Err(ServiceError::new(
+            ServiceErrorStatus::NotFound,
+            "Category 404 not found".to_string(),
+        ))
+    } else {
+        Ok(format!("[{}]Category list...", category_id))
+    }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::errors::{ServiceError, ServiceErrorStatus};
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn simulate_get_categories_returns_error_test() {
+        let result = simulate_get_categories(0);
+        assert!(result.is_err());
+        let error = result.unwrap_err();
+        assert_eq!(error.status, ServiceErrorStatus::BadRequest);
+        assert_eq!(error.message, "categories cannot be 0".to_string());
+    }
+
+    #[test]
+    fn simulate_get_categories_returns_not_found_error_test() {
+        let result = simulate_get_categories(404);
+        assert!(result.is_err());
+        let error = result.unwrap_err();
+        assert_eq!(error.status, ServiceErrorStatus::NotFound);
+        assert_eq!(error.message, "Category 404 not found".to_string());
+    }
+
+    #[test]
+    fn simulate_get_categories_returns_correct_test() {
+        let result = simulate_get_categories(12);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), String::from("[12]Category list..."));
+    }
+
+    #[test]
+    fn service_error_new_test() {
+        let error = ServiceError::new(
+            ServiceErrorStatus::BadRequest,
+            "Calling function is not operational".to_string(),
+        );
+        assert_eq!(error.status, ServiceErrorStatus::BadRequest);
+        assert_eq!(
+            error.message,
+            "Calling function is not operational".to_string()
+        );
+    }
+
+    #[test]
+    fn service_error_display_test() {
+        let error = ServiceError::new(
+            ServiceErrorStatus::BadRequest,
+            String::from("The resource path not found"),
+        );
+        assert_eq!(
+            format!("{}", error),
+            String::from("The resource path not found")
+        );
     }
 }
