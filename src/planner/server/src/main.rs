b3e5746d6962e552ac8c6655992fe866acb77f79
buraksenyurt <burakselimsenyurt@gmail.com>
2025-07-04 16:34:11 +0300

    Refactored route configurations.

--- src/planner/server/src/main.rs
+++ src/planner/server/src/main.rs
@@ -1,6 +1,6 @@
 mod api;
 
-use crate::api::actions::{create, delete, get, update};
+use crate::api::actions::{configure_routes, create, delete, get, update};
 use actix_web::web::{delete, get, post, put};
 use actix_web::{App, HttpServer};
 
@@ -8,24 +8,9 @@ use actix_web::{App, HttpServer};
 async fn main() -> std::io::Result<()> {
     dotenvy::dotenv().expect("Failed to load .env file");
     println!("Server started at http://localhost:3000");
-    HttpServer::new(|| {
-        App::new()
-            .route("/", get().to(get::fetch_all))
-            .route(
-                "filter/by/status/{status}",
-                get().to(get::fetch_all_by_status),
-            )
-            .route(
-                "/filter/by/size/{size}",
-                get().to(get::fetch_all_by_size_grater_than),
-            )
-            .route("/{title}", get().to(get::fetch_by_id))
-            .route("/", post().to(create::create_work_item))
-            .route("/", put().to(update::update_work_item))
-            .route("/{title}", delete().to(delete::delete_by_title))
-    })
-    .workers(8)
-    .bind("127.0.0.1:3000")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().configure(configure_routes))
+        .workers(8)
+        .bind("127.0.0.1:3000")?
+        .run()
+        .await
 }
